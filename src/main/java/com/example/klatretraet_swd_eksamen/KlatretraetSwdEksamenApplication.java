98e3b445fcf9332934375a5fea4bcb43e4047214
silasmj <74252789+silasmj@users.noreply.github.com>
2021-11-12 14:25:20 +0100

    Initial commit

---
+++ src/main/java/com/example/klatretraet_swd_eksamen/KlatretraetSwdEksamenApplication.java
@@ -0,0 +1,13 @@
+package com.example.klatretraet_swd_eksamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlatretraetSwdEksamenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KlatretraetSwdEksamenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/klatretraet_swd_eksamen/KlatretraetSwdEksamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.klatretraet_swd_eksamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlatretraetSwdEksamenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
