7f1421a85a8cd9cfec96dd09305846a107a18c3c
dwaGithub <78429825+dwaGithub@users.noreply.github.com>
2021-11-30 13:08:22 +0100

    emoloyee DTO og start på controller

---
+++ src/main/java/com/example/klatretraet_swd_eksamen/DTO/EmployeeDTO.java
@@ -0,0 +1,6 @@
+package com.example.klatretraet_swd_eksamen.DTO;
+
+public class EmployeeDTO {
+
+    public Iterable<String> employeeIds;
+}
--- src/main/java/com/example/klatretraet_swd_eksamen/controllers/EmployeeController.java
+++ src/main/java/com/example/klatretraet_swd_eksamen/controllers/EmployeeController.java
@@ -1,7 +1,37 @@
 package com.example.klatretraet_swd_eksamen.controllers;
 
+import com.example.klatretraet_swd_eksamen.models.Employee;
+import com.example.klatretraet_swd_eksamen.repositories.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
 public class EmployeeController {
+
+    @Autowired
+    EmployeeRepository employee;
+
+    @GetMapping("/employees")
+    public List<Employee> getEmployees(){
+        return employee.findAll();
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployeeById(@PathVariable Long id){
+        return employee.getById(id);
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee newEmployee){
+        return employee.save(newEmployee);
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public void deleteEmployeeById(@PathVariable Long id){
+        employee.deleteById(id);
+    }
+
 }
