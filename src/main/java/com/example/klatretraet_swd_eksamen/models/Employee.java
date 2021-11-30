657189ccf1dd2ea5e52c5df58470ce505ff36982
oliverbp99 <oliverbpetersen99@gmail.com>
2021-11-30 11:19:13 +0100

    Employee model created and pom.xml changed

---
+++ src/main/java/com/example/klatretraet_swd_eksamen/models/Employee.java
@@ -0,0 +1,26 @@
+package com.example.klatretraet_swd_eksamen.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String area;
+
+    @Column
+    private String image;
+
+    @Column
+    private double calculatedVacation;
+}
