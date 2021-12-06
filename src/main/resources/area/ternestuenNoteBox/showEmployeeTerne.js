a25cb7aaa5796e08ac8887c34d808f2d3722def4
silasmj <74252789+silasmj@users.noreply.github.com>
2021-12-06 11:46:58 +0100

    Den finder personer fra ternestuen

--- src/main/resources/area/ternestuenNoteBox/showEmployeeTerne.js
+++ src/main/resources/area/ternestuenNoteBox/showEmployeeTerne.js
@@ -1,22 +1,11 @@
-let filteredEmployees;
-let employees;
 const employeeDiv = document.getElementById("employee-div")
-let area = "Ternestuen";
-
 
 fetch(baseURL + "/employees")
     .then(response => response.json())
     .then(result => {
         let terneEmployees = result.filter(employee => employee.area && employee.area.name == 'Ternestuen');
-        console.log(terneEmployees)
+        createEmployeeCard(terneEmployees);
     })
-
-function handleFilteredEmployees(employee) {
-    employeeDiv.innerHTML = "";
-    employee.filter(employee => employee.area.name.toLowerCase().includes(area.toLowerCase())).map(createEmployeeCard());
-
-
-}
 
 function createEmployeeCard(employees) {
     const cardElement = document.createElement("div")
