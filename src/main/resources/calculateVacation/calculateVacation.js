32443a8ec977d622d8ba35fe7e8d26623fc4fa2a
dwaGithub <78429825+dwaGithub@users.noreply.github.com>
2021-12-09 11:13:49 +0100

    Start på udregning af ferie

---
+++ src/main/resources/calculateVacation/calculateVacation.js
@@ -0,0 +1,8 @@
+let sum;
+let vacationBalance;
+let usedVacation;
+let newVacationBalance;
+
+function calculateVacation(sum, vacationBalance, usedVacation, newVacationBalance){
+    document.getElementById(vacationBalance)
+}
