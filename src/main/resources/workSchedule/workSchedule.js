7aed00dd953776e20c9240cfdb611e9f0a22cd66
guob0001 <78424415+guob0001@users.noreply.github.com>
2021-12-09 13:10:14 +0100

    tabel vagtplan (opdateret v)

--- src/main/resources/workSchedule/workSchedule.js
+++ src/main/resources/workSchedule/workSchedule.js
@@ -2,29 +2,26 @@ function myFunction() {
     var table = document.getElementById("work-schedule-tbody");
     var row = table.insertRow(1);
     var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(0);
-    var cell3 = row.insertCell(1)
-    var cell4 = row.insertCell(2)
-    var cell5 = row.insertCell(3)
-    var cell6= row.insertCell(4);
-    var cell7 = row.insertCell(5);
-    var cell8 = row.insertCell(6)
-    var cell9 = row.insertCell(7)
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2)
+    var cell4 = row.insertCell(3)
+    var cell5 = row.insertCell(4)
+    var cell6= row.insertCell(5);
+    var cell7 = row.insertCell(6);
+    var cell8 = row.insertCell(7)
+    var cell9 = row.insertCell(8)
 
-    cell1.innerHTML = "x";
-    cell2.innerHTML = "Medarbejder navn";
-    cell3.innerHTML = "x";
-    cell4.innerHTML = "x";
-    cell5.innerHTML = "x";
-    cell6.innerHTML = "x";
-    cell7.innerHTML = "x";
-    cell8.innerHTML = "x";
-    cell9.innerHTML = "x";
+    cell1.innerHTML = "Medarbejder navn";
+    cell2.innerHTML = "08:00-15:30";
+    cell3.innerHTML = "06:30-12:30";
+    cell4.innerHTML = "09:15-17:00";
+    cell5.innerHTML = "08:00-16:00";
+    cell6.innerHTML = "08:00-15:30";
+    cell7.innerHTML = "06:45-12:45";
+    cell8.innerHTML = "09:00-16:30";
+    cell9.innerHTML = "08:00-16:00";
+
+
 
 }
-/*
- function myFunctionn() {
-    var row = document.getElementById("myRow");
-    var x = row.insertCell(-1);
-    x.innerHTML = "Ny tid";
-}*/
+
