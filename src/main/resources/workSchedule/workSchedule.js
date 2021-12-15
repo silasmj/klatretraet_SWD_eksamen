644c68d8e3307d458e1ba92058f110c30d53d3ad
dwaGithub <78429825+dwaGithub@users.noreply.github.com>
2021-12-15 14:13:48 +0100

    Der kan tilføjes workschedule objekt i front-end og back-end.

--- src/main/resources/workSchedule/workSchedule.js
+++ src/main/resources/workSchedule/workSchedule.js
@@ -35,19 +35,31 @@ function constructWorkscheduleTableRow(workscheduleTableRow, workschedule) {
         </td>
     `;
 }
-function addRowToTable() {
-    var table = document.getElementById("workschedule-tbody");
-    var row = table.insertRow(0);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell4 = row.insertCell(3);
-    var cell5 = row.insertCell(4);
-    var cell6 = row.insertCell(5);
-    cell1.innerHTML = "null";
-    cell2.innerHTML = "null";
-    cell3.innerHTML = "null";
-    cell4.innerHTML = "null";
-    cell5.innerHTML = "null";
-    cell6.innerHTML = "null";
+
+function createNewWorkSchedule(){
+    var weekYear = fetchWeek(offset);
+
+    const newWorkScheduleRow = {
+        year: weekYear.year,
+        weekNumber: weekYear.number,
+        employeeName: "Ikke udfyldt",
+        monday: "Ikke udfyldt",
+        tuesday: "Ikke udfyldt",
+        wednesday: "Ikke udfyldt",
+        thursday: "Ikke udfyldt",
+        friday: "Ikke udfyldt"
+    };
+    fetch(baseURL + "/workSchedule", {
+        method: "POST",
+        headers: {"Content-type": "application/json; charset=UTF-8"},
+        body: JSON.stringify(newWorkScheduleRow)
+    }).then(response => {
+        if (response.status === 200) {
+            location.reload();
+            console.log(response)
+            fetchSchedule();
+        } else {
+            console.log("Vagtplan ikke oprettet", response.status);
+        }
+    });
 }
