63c2c51beb9f38435436635eb1da1e850a269ebf
silasmj <74252789+silasmj@users.noreply.github.com>
2021-12-15 21:39:22 +0100

    Man kan nu scrolle til andre uger, og få loadet vagtplan for den uge

--- src/main/resources/workSchedule/workSchedule.js
+++ src/main/resources/workSchedule/workSchedule.js
@@ -63,3 +63,18 @@ function createNewWorkSchedule(){
         }
     });
 }
+
+document.querySelectorAll("#work-schedule-tbody").forEach(function(node){
+    node.ondblclick=function(){
+        var val=this.innerHTML;
+        var input=document.createElement("input");
+        input.value=val;
+        input.onblur=function(){
+            var val=this.value;
+            this.parentNode.innerHTML=val;
+        }
+        this.innerHTML="";
+        this.appendChild(input);
+        input.focus();
+    }
+});
