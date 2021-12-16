a76b753a4e6005295c586241c4b3ecd7b2233f15
dwaGithub <78429825+dwaGithub@users.noreply.github.com>
2021-12-16 14:18:36 +0100

    Uger + årstal i vagtplan er fixed (semi).

--- src/main/resources/workSchedule/dateHandling.js
+++ src/main/resources/workSchedule/dateHandling.js
@@ -14,24 +14,38 @@ function getWeekNumber(offset) {
         tdt.setMonth(0, 1 + ((4 - tdt.getDay()) + 7) % 7);
     }
     var result = 1 + Math.ceil((firstThursday - tdt) / 604800000) + offset;
+    if (result > 52){
+        var yearOffset = yearOffsetCalculator(result)
+        dt.setFullYear(dt.getFullYear() +  yearOffset)
+
+        result = result % 52;
+        if(result === 0){
+           result = 52;
+        }
+    }
     range.innerHTML = `Uge: ` + result + `, År: ` + dt.getFullYear();
 }
+
+function yearOffsetCalculator(offset) {
+    return Math.floor(offset / 52);
+}
+
 function forward() {
     offset = offset + 1;
     getWeekNumber(offset);
-    fetchSchedule(offset);
 }
 
 function backward() {
     offset = offset - 1;
     getWeekNumber(offset);
-    fetchSchedule(offset);
 }
+
 
 window.onload = function() {
     getWeekNumber();
     fetchSchedule()
 }
+
 
 function fetchWeek(offset) {
     offset = offset || 0;
@@ -51,11 +65,11 @@ function fetchWeek(offset) {
     }
 }
 
-function fetchSchedule(offset) {
+function fetchSchedule() {
     fetch(baseURL + "/workSchedule")
         .then(response => response.json())
         .then(result => {
-            var weekYear = fetchWeek(offset);
+            var weekYear = fetchWeek();
             console.log(weekYear.number + " " + weekYear.year)
             console.log(result)
             const filtered = result.filter(schedule => schedule.year === weekYear.year && schedule.weekNumber === weekYear.number)
