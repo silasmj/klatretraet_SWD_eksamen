1fd93fd83f13255518c365450969692b263a563c
silasmj <74252789+silasmj@users.noreply.github.com>
2021-12-17 13:19:25 +0100

    fixed stuer

--- src/main/resources/area/andestuenNoteBox/showEmployeeAnde.js
+++ src/main/resources/area/andestuenNoteBox/showEmployeeAnde.js
@@ -1,7 +1,20 @@
+const employeeDiv = document.getElementById("employee-div")
+
 fetch(baseURL + "/employees")
     .then(response => response.json())
     .then(result => {
-        let andeEmployees = result.filter(employee => employee.area && employee.area.name == 'Andestuen')
-        console.log(andeEmployees)
+        const andeEmployees = result.filter(employee => employee.area && employee.area.name === 'Andestuen');
+        andeEmployees.map(createEmployeeCard);
     })
+
+function createEmployeeCard(employees) {
+    console.log(employees)
+    const cardElement = document.createElement("div")
+
+    cardElement.innerHTML = `
+        <span><img class="employee-img" src="../../img/abccc.png"></span>
+        <span>${employees.name}</span>
+    `;
+    employeeDiv.appendChild(cardElement);
+}
 
--- src/main/resources/area/vibestuenNoteBox/showEmployeeVibe.js
+++ src/main/resources/area/vibestuenNoteBox/showEmployeeVibe.js
@@ -1,7 +1,20 @@
+const employeeDiv = document.getElementById("employee-div")
+
 fetch(baseURL + "/employees")
     .then(response => response.json())
     .then(result => {
-        let vibeEmployees = result.filter(employee => employee.area && employee.area.name == 'Vibestuen');
-
-        console.log(vibeEmployees)
+        const vibeEmployees = result.filter(employee => employee.area && employee.area.name === 'Vibestuen');
+        vibeEmployees.map(createEmployeeCard);
     })
+
+function createEmployeeCard(employees) {
+    console.log(employees)
+    const cardElement = document.createElement("div")
+
+    cardElement.innerHTML = `
+        <span><img class="employee-img" src="../../img/abccc.png"></span>
+        <span>${employees.name}</span>
+    `;
+    employeeDiv.appendChild(cardElement);
+}
+
